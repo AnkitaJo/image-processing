2b5a339facd5c3f1473f89c9ff3da98cad4fcdd8
AnkitaJo <ankita.joshi1591@gmail.com>
2018-03-20 06:17:49 -0400

    final changes

--- project-1/images.py
+++ project-1/images.py
@@ -2,7 +2,8 @@
 
 import numpy as np
 import cv2
-import Image, ImageDraw
+import copy
+#from PIL import Image
 import sys
 
 
@@ -10,62 +11,135 @@ import sys
 #loading a color image in gray-scale
 #img = cv2.imread('../../Desktop/House_sparrow04.jpg',1)
 def read_image(img):
-	oimg = cv2.imread(img)
-	#oimg = cv2.resize(image, (0, 0), None, .25, .25)
+	image = cv2.imread(img)
+	oimg = cv2.resize(image, (256, 256))
 	return oimg
 
 def gray_scale(img):
 	#display the gray scale image
 	grey = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+	print(grey.shape)
+	cv2.imwrite('gray.jpg', grey)
+	image = cv2.imread("gray.jpg")
+	oimg = cv2.resize(image, (256, 256))
+	print(oimg.shape)
+	return oimg
 
-	return grey
 
-def display(img1,img2):
+def noiseGaussian(img):
 
-	
-	image1 = cv2.resize(img1, (0, 0), None, .25, .25)
-	image2 = cv2.resize(img2, (0, 0), None, .25, .25)
-	numpy_horizontal = np.hstack((image1, image2))
-	numpy_horizontal_concat = np.concatenate((image1, image2), axis=1)
-	cv2.imshow('Result', numpy_horizontal_concat)
-	
-	
+	row,col,ch= img.shape
+	mean = 0
+	var = 100
+	sigma = var**0.5
+	gauss = np.random.normal(mean,sigma,(row,col,ch))
+	gauss = gauss.reshape(row,col,ch)
+	gauss = gauss.astype(int)
+	noisy = img + gauss
+	cv2.imwrite('nois.jpg', noisy)
+	nn = cv2.imread("nois.jpg")
+	"""
+	n = noisy.astype(int)
+	for i in n:
+			for j in i:
+				for k in j:
+					if (k < 0) or (k > 255):
+						print(str(j) + " ")
+	#np.savetxt("res.txt", n)
+	"""
+	return nn
 
-#Question 2
-#corrupting the image by noise
-#1. Gaussian noise
+def noiseSaltPepper(img):
+	row,col,ch = img.shape
+	s_vs_p = 0.5
+	amount = 0.004
+	out = np.copy(img)
+	# Salt mode
+	num_salt = np.ceil(amount * img.size * s_vs_p)
+	coords = [np.random.randint(0, i - 1, int(num_salt))
+		for i in img.shape]
+	out[coords] = 1
+
+	# Pepper mode
+	num_pepper = np.ceil(amount* img.size * (1. - s_vs_p))
+	coords = [np.random.randint(0, i - 1, int(num_pepper))
+		for i in img.shape]
+	out[coords] = 0
+	return out
+
+def smoothening(img):
+	kernel = np.ones((9,9),np.float32)/81
+	dst = cv2.filter2D(img,-1,kernel)
+	return(dst)
+
+def medianFilter(img):
+	median = cv2.medianBlur(img,5)
+	return median
+
+
+def thresholding(img):
+	#o2 = gray_scale(img)
+	#ret,thresh1 = cv2.threshold(o2,127,255,cv2.THRESH_BINARY)
+	return img
 
 """
-row,col,ch= img.shape
-mean = 0
-var = 0.05
-sigma = var**0.5
-gauss = np.random.normal(mean,sigma,(row,col,ch))
-gauss = gauss.reshape(row,col,ch)
-noisy = img + gauss
-cv2.imshow('image',noisy)
-cv2.waitKey(0)
-cv2.destroyAllWindows()
+def ithresholding(img):
+	o2 = gray_scale(img)
+"""
 
-#2. Salt-pepper noise introduction
-row,col,ch = img.shape
-s_vs_p = 0.5
-amount = 0.004
-out = np.copy(img)
-# Salt mode
-num_salt = np.ceil(amount * img.size * s_vs_p)
-coords = [np.random.randint(0, i - 1, int(num_salt))
-	for i in img.shape]
-out[coords] = 1
+"""
 
-# Pepper mode
-num_pepper = np.ceil(amount* img.size * (1. - s_vs_p))
-coords = [np.random.randint(0, i - 1, int(num_pepper))
-	for i in img.shape]
-out[coords] = 0
-cv2.imshow('image',out)
-cv2.waitKey(0)
-cv2.destroyAllWindows()
+def labelComponents(img):
+	o2 = gray_scale(img)
+	ret,thresh1 = cv2.threshold(o2,127,255,cv2.THRESH_BINARY)
+	ret1, labels = cv2.connectedComponents(thresh1)
+
+	label_hue = np.uint8(179*labels/np.max(labels))
+	blank_ch = 255*np.ones_like(label_hue)
+	labeled_img = cv2.merge([label_hue, blank_ch, blank_ch])
+
+	labeled_img = cv2.cvtColor(labeled_img, cv2.COLOR_HSV2BGR)
+	labeled_img[label_hue==0] = 0
+	return labeled_img
+"""
+
+
+
+def display1(img1):
+	cv2.imshow('Result', img1)
+	cv2.waitKey(0)
+	cv2.destroyAllWindows()
+
+def display(img1,img2):
+	
+
+	#display1(img1)
+	#display1(img2)
+
+
+	numpy_horizontal = np.hstack((img1, img2))
+	#print(numpy_horizontal.shape)
+	#numpy_horizontal_concat = np.concatenate((img1, img2), axis=1)
+	cv2.imshow('Result', numpy_horizontal)
+	cv2.waitKey(0)
+	cv2.destroyAllWindows()
+
+
+	#display1(img1)
+	#display1(img2)
+	
+
+
+
+
+	
+	
+
+
+
+	
+
+"""
 
 #3. Poisson noise introduction
 vals = len(np.unique(img))
@@ -85,64 +159,27 @@ cv2.waitKey(0)
 cv2.destroyAllWindows()
 """
 
-"""
-#Question 3
-#Smoothening an image using averaging technique
-img = cv2.imread('../../Desktop/House_sparrow04.jpg',1)
-kernel = np.ones((9,9),np.float32)/81
-dst = cv2.filter2D(img,-1,kernel)
-cv2.imshow('image',dst)
-cv2.waitKey(0)
-cv2.destroyAllWindows()
-"""
-"""
-#Question 4
-#Applying Median filtering to an image
-img = cv2.imread('../../Desktop/House_sparrow04.jpg',1)
-median = cv2.medianBlur(img,5)
-cv2.imshow('image1',median)
-cv2.imshow('image2',img)
-cv2.waitKey(0)
-cv2.destroyAllWindows()
-"""
-
-"""
-#Question 5
-#Converting a picture to binary using simple thresholding technique
-
-#image should be a gray scale image to use this technique
-img = cv2.imread('../../Desktop/House_sparrow04.jpg',0)
-ret,thresh1 = cv2.threshold(img,127,255,cv2.THRESH_BINARY)
-cv2.imshow('image',thresh1)
-cv2.waitKey(0)
-cv2.destroyAllWindows()
-
-#Question 6
-#Convert a picture to binary using the P-Tile Method
-#def pTile(img,x):
 
 
 
 
-#Question 7
-#Converting a picture to binary using the Iterative Thresholding method
 
-#Question 8
-#Label Compponents of a black and white image
-def labelComponents(img):
-	#convert the image in binary
-	#
-
-
-#Question 10
-#Other functions: Histogram
-"""
 def main():
 	#Open the image
 
 	o1 = read_image(sys.argv[1])
-	o2 = gray_scale(o1)
-	display(o1,o2)
+	sample = copy.deepcopy(o1)
+	o2 = gray_scale(sample)	
+	o3 = noiseGaussian(sample)
+	o4 = noiseSaltPepper(sample)
+	o5 = smoothening(sample)
+	o6 = medianFilter(sample)
+	o7 = thresholding(sample)
+	#o8 = labelComponents(sample)
+	#print(o2.shape)
+	
+	#display(o1,o2)
+
 
 
 if __name__ == "__main__": main()
